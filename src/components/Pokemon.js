1248a707faf9c893590158647aaaa8ae48f8eff4
MusicalProf <krishon2006@gmail.com>
2020-11-09 06:48:26 -0600

    Added functionality to fetch button

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Pokemon = (props) => {
   const [pokemon, setPokemon] = useState([]);
+  const [capture, setCapture] = useState(false);
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
@@ -9,16 +10,41 @@ const Pokemon = (props) => {
       .then((response) => setPokemon(response.results));
   }, []);
 
+  const capture807 = (e) => {
+    e.preventDefault();
+    if (capture === false) {
+      setCapture(true);
+    } else {
+      setCapture(false);
+    }
+  };
+
   return (
     <div className="container w-50 border rounded-lg mt-3">
-      <h4>Below is a list of 807 pokemon</h4>
-      {/* <button onClick={capture807}>Fetch Pokemon</button> */}
+      <h4>Click below for a list of 807 pokemon</h4>
+      <button className="btn btn-primary" onClick={capture807}>
+        Fetch Pokemon
+      </button>
       <hr />
-      
-      {pokemon.length > 0 &&
-        pokemon.map((poke, index) => {
-          return <div key={index}>Name: {poke.name} Json URL: <a href={poke.url}>{poke.name}</a></div>;
-        })}
+
+      <table className="table table-striped col-10 shadow">
+        <tr>
+          <th>Poke #</th>
+          <th>Poke Name</th>
+        </tr>
+
+        {capture
+          ? pokemon.length > 0 &&
+            pokemon.map((poke, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{poke.name}</td>
+                </tr>
+              );
+            })
+          : ""}
+      </table>
     </div>
   );
 };
