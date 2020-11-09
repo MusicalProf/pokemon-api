38c79b1d09c9e3ef9c1b4b02b9a483625d27b43a
MusicalProf <krishon2006@gmail.com>
2020-11-09 07:02:28 -0600

    Updated Pokemon Component to use Axios

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Pokemon = (props) => {
   const [pokemon, setPokemon] = useState([]);
   const [capture, setCapture] = useState(false);
 
+  // Using only useEffect hook with fetch
+  //   useEffect(() => {
+  //     fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+  //       .then((response) => response.json())
+  //       .then((response) => setPokemon(response.results));
+  //   }, []);
+
+  // Updating to use Axios along with useEffect
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
-      .then((response) => response.json())
-      .then((response) => setPokemon(response.results));
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then((response) => setPokemon(response.data.results));
   }, []);
 
   const capture807 = (e) => {
@@ -27,23 +36,26 @@ const Pokemon = (props) => {
       </button>
       <hr />
 
-      <table className="table table-striped col-10 shadow">
-        <tr>
-          <th>Poke #</th>
-          <th>Poke Name</th>
-        </tr>
-
-        {capture
-          ? pokemon.length > 0 &&
-            pokemon.map((poke, index) => {
-              return (
-                <tr>
-                  <td>{index + 1}</td>
-                  <td>{poke.name}</td>
-                </tr>
-              );
-            })
-          : ""}
+      <table className="table table-success thead-dark table-striped shadow">
+        <thead className="thead-dark">
+            <tr>
+              <th>Poke #</th>
+              <th>Poke Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            {capture
+              ? pokemon.length > 0 &&
+                pokemon.map((poke, index) => {
+                  return (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{poke.name}</td>
+                    </tr>
+                  );
+                })
+              : ""}
+        </tbody>
       </table>
     </div>
   );
